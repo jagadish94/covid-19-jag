7f34c91542070a1459b374c504aa3a1b54ca096b
jagadish kumar <ponna.jagadish1234@gmail.com>
2020-05-04 14:40:18 +0530

    portchange

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const request = require('request')
 const path = require('path')
 const app = express()
-const port =procees.env.PORT || 3000;
+const port =process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname,'../public')
 console.log(publicDirectoryPath)
 app.use(express.static(publicDirectoryPath))
